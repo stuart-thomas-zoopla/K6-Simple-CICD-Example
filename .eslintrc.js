d56a0ba7ff8742bdf4f5cb9366a215e15a20c9a6
stuart-thomas-zoopla <stuart.thomas@zoopla.co.uk>
2023-08-23 18:00:42 +0100

    Switch eslint rules for eslint-plugin-k6-linting-rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,12 +16,5 @@ module.exports = {
         "__ITER": "readonly"
     },
     "rules": {
-        "import/no-unresolved": 0, // k6 is actually golang, can't really import it
-        "no-restricted-globals": 0, // required by k6, e.g. "init" context
-        "import/extensions": 0, // .js ending is ok
-        "no-prototype-builtins": 0, // k6 assertions
-        "semi": 1,
-        "quotes": 2
-
     }
 }
---
+++ eslint.config.js
@@ -0,0 +1,5 @@
+// eslint.config.js
+"use strict";
+
+// Import the plugin downloaded from npm
+const eslintk6 = require("eslint-plugin-k6-linting-rules");
